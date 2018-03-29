3e4aa7a989223e798bb51d7e3dce7a0008cc4bb1
quandtifier <quandtifier@gmail.com>
2018-03-29 11:11:37 -0700

    Added callback method overrides.

--- app/src/main/java/edu/tacoma/uw/css/quandm/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/quandm/myfirstapp/DisplayMessageActivity.java
@@ -20,4 +20,42 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
     }
+
+    /**Makes the activity visible to the user
+     * We may register a BroadcastReceiver here
+     * Once callback finishes the system invokes onResume()*/
+    @Override public void onStart() {
+        super.onStart();
+    }
+
+    /**State in which the app interacts with the user
+     * Stays in this state until focus is taken away
+     * An interruption send the activity into Pause state by invoking onPause()
+     * Implement onResume() to initialize components released during onPause()*/
+    @Override public void onResume() {
+        super.onResume();
+
+    }
+
+    /**Called whenever user leaves an activity
+     * Can be used to release resources in order to save energy or performance
+     * If activity visibility reduces to invisible then onStop() is invoked*/
+    @Override public void onPause() {
+        super.onPause();
+    }
+
+    /**Activity is no longer visible or is completed running and is about to terminate
+     * Release nearly all resources here
+     * Perform CPU intensive shutdown operations here*/
+    @Override public void onStop() {
+        super.onStop();
+    }
+
+    /**Final call that an activity receives
+     * only called by someone has called finish() or system my temporarily destroy to save space
+     * Can distinguish between the two cases by calling isFinishing()
+     * Orientation changes also call this method then onCreate() to recreate the process*/
+    @Override public void onDestroy() {
+        super.onDestroy();
+    }
 }
--- app/src/main/java/edu/tacoma/uw/css/quandm/myfirstapp/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/quandm/myfirstapp/MainActivity.java
@@ -8,11 +8,49 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_MESSAGE = "edu.tacoma.uw.css.quandm.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Makes the activity visible to the user
+     * We may register a BroadcastReceiver here
+     * Once callback finishes the system invokes onResume()*/
+    @Override public void onStart() {
+        super.onStart();
+    }
+
+    /**State in which the app interacts with the user
+     * Stays in this state until focus is taken away
+     * An interruption send the activity into Pause state by invoking onPause()
+     * Implement onResume() to initialize components released during onPause()*/
+    @Override public void onResume() {
+        super.onResume();
+
+    }
+
+    /**Called whenever user leaves an activity
+     * Can be used to release resources in order to save energy or performance
+     * If activity visibility reduces to invisible then onStop() is invoked*/
+    @Override public void onPause() {
+        super.onPause();
+    }
+
+    /**Activity is no longer visible or is completed running and is about to terminate
+     * Release nearly all resources here
+     * Perform CPU intensive shutdown operations here*/
+    @Override public void onStop() {
+        super.onStop();
+    }
+
+    /**Final call that an activity receives
+     * only called by someone has called finish() or system my temporarily destroy to save space
+     * Can distinguish between the two cases by calling isFinishing()
+     * Orientation changes also call this method then onCreate() to recreate the process*/
+    @Override public void onDestroy() {
+        super.onDestroy();
     }
 
     /**Called from user tap of the Send button*/
